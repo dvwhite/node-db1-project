83dad95484bf76027eebfc67e8ed35d663cbc9db
David White <dwhite0627@gmail.com>
2020-04-12 17:49:26 -0400

    Add routes and logger middleware to server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
 const server = express();
 
+// Middleware
 server.use(express.json());
+server.use(logger);
+
+// Routes
+const accountRoutes = require('./../data/accounts/accountRouter');
+server.use('/accounts', accountRoutes);
+
+// Custom middleware
+
+// Logger
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+
+  next();
+}
+
+server.get("/", (req, res) => {
+  res.status(200).json("<h2>Welcome to the Accounts API!</h2>")
+})
 
 module.exports = server;
